cacd244c1d27350ba90c84190cdef5300564087d
lostsquirrel <songli1988@gmail.com>
2014-06-11 08:09:16 +0800

    添加Poll后台管理

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 # Register your models here.
+from polls.models import Poll
+
+class PollAdmin(admin.ModelAdmin):
+   fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+admin.site.register(Poll, PollAdmin)
